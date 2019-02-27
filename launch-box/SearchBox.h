f31d37fff9a722886bc429d276748d63d2c5365a
zhenhuanlee <zhenhuanlee@outlook.com>
2019-02-27 14:27:16 +0800

    ionit

---
+++ launch-box/SearchBox.h
@@ -0,0 +1,17 @@
+//
+//  SearchBox.h
+//  launch-box
+//
+//  Created by jude on 2019/2/27.
+//  Copyright © 2019 jude. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchBox : NSTextField
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ launch-box/SearchBox.m
@@ -0,0 +1,19 @@
+//
+//  SearchBox.m
+//  launch-box
+//
+//  Created by jude on 2019/2/27.
+//  Copyright © 2019 jude. All rights reserved.
+//
+
+#import "SearchBox.h"
+
+@implementation SearchBox
+
+- (void)drawRect:(NSRect)dirtyRect {
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
